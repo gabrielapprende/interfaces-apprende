dd9a75654530d62869c0f65e4d16b7e8f553c982
Gabriel Cervantes <151411119+gabrielapprende@users.noreply.github.com>
2025-02-21 10:44:49 -0500

    Create StudentHeader.ts

---
+++ StudentHeader.ts
@@ -0,0 +1,10 @@
+export interface IStudentHeader {
+	name: string;
+	lastname: string;
+	level: string;
+	nee: string;
+	entryDate: string;
+	diagnoses: string[];
+	birthDate?: string;
+	age?: string;
+}
