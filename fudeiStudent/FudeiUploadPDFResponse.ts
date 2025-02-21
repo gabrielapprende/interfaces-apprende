4b1b3e6da4147fa40a75c79f3018e036ad5ca8f5
Gabriel Cervantes <151411119+gabrielapprende@users.noreply.github.com>
2025-02-21 11:06:21 -0500

    IFudeiUploadPDFResponse

---
+++ fudeiStudent/FudeiUploadPDFResponse.ts
@@ -0,0 +1,8 @@
+export interface IFudeiUploadPDFResponse {
+	// TODO: Cambiar a la respuesta real
+	hub: string;
+	type: string;
+	fileName: string;
+	fileContentType: string;
+	filePath: string;
+}
