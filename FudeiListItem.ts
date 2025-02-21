7c14c3036eb4e7f608e1b93cffea7fc67751e039
Gabriel Cervantes <151411119+gabrielapprende@users.noreply.github.com>
2025-02-21 10:43:29 -0500

    Create FudeiListItem.ts

---
+++ FudeiListItem.ts
@@ -0,0 +1,20 @@
+export interface FudeiListITem {
+	Id: string;
+	diagnoses: string[];
+	entryDate: string;
+	fudei: boolean;
+	fudeiId: string;
+	lastname: string;
+	level: string;
+	memberId: string;
+	name: string;
+	nee: string;
+	pie: boolean;
+	pai: string;
+	responsible: string;
+	schoolId: number;
+	status: string;
+	studentId: string;
+	supportPlanId: string;
+	typeNee: string;
+}
